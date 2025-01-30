f46ff68a03e149b5542c2f551d3b1975e13a9c91
lyra <mk.lyra.j@gmail.com>
2025-01-30 21:38:52 +0900

    style: todolist input 영역 스타일드 컴포넌트로 구현

--- rtx_todolist01/src/App.jsx
+++ rtx_todolist01/src/App.jsx
@@ -1,8 +1,66 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+// styled-components
+const MainContainer = styled.div`
+  max-width: 600px;
+  background-color: #f4f4f9;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  border-radius: 10px;
+  margin: 50px auto;
+`;
+
+const Header = styled.h1`
+  text-align: center;
+  color: #33333;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+const Input = styled.input`
+  width: 80%;
+  padding: 10px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  padding: 10px 16px;
+  font-size: 14px;
+  background-color: #3db793;
+  color: #ffffff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0c6349;
+  }
+`;
 
 function App() {
-  return <>초기 셋팅</>;
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <MainContainer>
+      <Header>rtk Todo List</Header>
+      <InputContainer>
+        <Input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="새로운 할 일을 입력하세요."
+        />
+        <Button>add+</Button>
+      </InputContainer>
+    </MainContainer>
+  );
 }
 
 export default App;
