241a1d5074c503cf5fcf49ee4a527f463e856622
lyra <mk.lyra.j@gmail.com>
2025-01-30 23:31:45 +0900

    feat: 할일 추가, 삭제 기능 구현

--- rtx_todolist01/src/App.jsx
+++ rtx_todolist01/src/App.jsx
@@ -1,5 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+
+function App() {
+  // 전체 투두 목록 관리
+  const [todos, setTodos] = useState([]);
+  // 투두 작성 input
+  const [inputTodo, setInputTodo] = useState("");
+
+  // 로컬스토리지에서 할 일 목록 불러오기, 첫 렌더링시에만
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      setTodos(savedTodos);
+    }
+  }, []);
+
+  // 로컬스토리지에 할 일 목록 저장
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAddTodo = () => {
+    const newTodo = {
+      id: Date.now(),
+      text: inputTodo,
+    };
+
+    // 아무값도 입력되지 않았다면 추가 x
+    if (inputTodo.trim() === "") {
+      alert(`할 일을 입력해주세요~`);
+      return;
+    }
+
+    setTodos([...todos, newTodo]);
+    setInputTodo("");
+    alert(`새로운 할 일이 추가되었습니다.`);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const deleteTodo = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    setTodos(deleteTodo);
+    alert(`할 일이 삭제되었습니다.`);
+  };
+
+  return (
+    <MainContainer>
+      <Header>rtk Todo List</Header>
+      {/* 할 일 form */}
+      <InputContainer>
+        <Input
+          type="text"
+          value={inputTodo}
+          onChange={(e) => setInputTodo(e.target.value)}
+          placeholder="새로운 할 일을 입력하세요."
+        />
+        <Button onClick={handleAddTodo}>add+</Button>
+      </InputContainer>
+
+      {/* 할 일 목록 , 여러줄을 return시 ()묶는것 잊지말자!!*/}
+      <TodoList>
+        {todos.map((todo) => {
+          return (
+            <TodoItem key={todo.id}>
+              <TodoText>{todo.text}</TodoText>
+              <DeleteButton onClick={() => handleDeleteTodo(todo.id)}>
+                del-
+              </DeleteButton>
+            </TodoItem>
+          );
+        })}
+      </TodoList>
+    </MainContainer>
+  );
+}
+
+export default App;
 
 // styled-components
 const MainContainer = styled.div`
@@ -13,7 +91,7 @@ const MainContainer = styled.div`
 
 const Header = styled.h1`
   text-align: center;
-  color: #33333;
+  color: #333333;
 `;
 
 const InputContainer = styled.div`
@@ -43,24 +121,37 @@ const Button = styled.button`
   }
 `;
 
-function App() {
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
+const TodoList = styled.ul`
+  list-style-type: none;
+  padding: 10px;
+`;
 
-  return (
-    <MainContainer>
-      <Header>rtk Todo List</Header>
-      <InputContainer>
-        <Input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="새로운 할 일을 입력하세요."
-        />
-        <Button>add+</Button>
-      </InputContainer>
-    </MainContainer>
-  );
-}
+const TodoItem = styled.li`
+  background-color: white;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-export default App;
+const TodoText = styled.span`
+  font-size: 16px;
+  color: #333;
+`;
+
+const DeleteButton = styled.button`
+  padding: 10px 16px;
+  font-size: 14px;
+  background-color: #e03f2dd2;
+  color: #ffffff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #c0392b;
+  }
+`;
